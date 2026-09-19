96525e7789a2fe50191ed311a7f7a1be722b1e42
Deepak Kumar <deepakkumarnd@gmail.com>
2026-09-19 21:43:03 +0530

    Optimized the O(n^3) solution v2 to O(n^2) by caching the results of comparison

--- src/leetcode/5_longest_palindrom_substr_v2.rb
+++ src/leetcode/5_longest_palindrom_substr_v2.rb
@@ -5,14 +5,25 @@
 module LongestPalindromeV2
   extend self
 
-  def palindrome?(s, start_pos, end_pos)
-    return false if start_pos > end_pos
+  def palindrome?(s, start_pos, end_pos, verified_cache)
+    length = end_pos - start_pos + 1
+    key = length.odd? ? :odd : :even
 
-    while start_pos <= end_pos
-      return false if s[start_pos] != s[end_pos]
+    mid = (start_pos + end_pos) / 2
 
-      start_pos += 1
-      end_pos -= 1
+    left, right = if verified_cache[key].key?(mid)
+                    a, b = verified_cache[key][mid]
+                    [a - 1, b + 1]
+                  else
+                    length.odd? ? [mid, mid] : [mid, mid + 1]
+                  end
+
+    while left >= start_pos && right <= end_pos
+      return false if s[left] != s[right]
+
+      verified_cache[key][mid] = [left, right]
+      left -= 1
+      right += 1
     end
 
     true
@@ -21,13 +32,16 @@ module LongestPalindromeV2
   def longest_palindrome(s)
     return s if s.empty? || s.length == 1
 
-    (s.length..1).step(-1).each do |length|
+    verified_cache = { odd: {}, even: {} }
+    longest = ''
+
+    (1..s.length).each do |length|
       (0..(s.length - length)).each do |start|
-        return s.slice(start, length) if palindrome?(s, start, start + length - 1)
+        longest = s.slice(start, length) if palindrome?(s, start, start + length - 1, verified_cache)
       end
     end
 
-    s[0]
+    longest
   end
 end
 
